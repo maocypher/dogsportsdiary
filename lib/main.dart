6824043e2347d4d4c4f3a80bb76fc7cc406c1263
maocypher <maocypher@gmail.com>
2024-11-23 09:01:09 +0100

    #75 only portraitmode

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:dog_sports_diary/app/app.dart';
 import 'package:dog_sports_diary/core/di/service_provider.dart';
 import 'package:dog_sports_diary/core/services/hive_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   await HiveService.initAsync();
 
